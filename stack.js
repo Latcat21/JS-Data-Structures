ea9450d727dfd22824fdac86856ac799acddddd3
Brian <Brian@Brians-MacBook-Air.local>
2020-05-29 14:07:32 -0500

    finished

--- stack.js
+++ stack.js
@@ -4,6 +4,7 @@ class Stack {
     this.count = 0
   }
 
+  
   // Add element to top of stack
   push(element){
     this.items[this.count] = element
@@ -12,6 +13,7 @@ class Stack {
     return this.count - 1
   }
 
+  
   // Return and remove top element in stack
   // Return undefined if stack is epmty
   pop(){
@@ -21,13 +23,54 @@ class Stack {
     console.log(`${deleteItem} removed`)
     return deleteItem
   }
+
+  // Check top element in stack
+  peek(){
+    console.log(`top element is ${this.items[this.count -1]} `)
+    return this.items[this.count - 1]
+  }
+
+  // Check if stack is empty
+  isEmpty(){
+    console.log(this.count == 0 ? 'Stack is empty' : 'Stack is not empty')
+    return this.count == 0
+  }
+
+  // Check size of stack
+  size(){
+    console.log(`${this.count} elements in stack`)
+    return this.count
+  }
+
+  //print elements in stack
+  print(){
+    let str = ''
+    for(let i = 0; i < this.count; i++){
+      str += this.items[i] + ' '
+    }
+    return str
+  }
+
+  // clear stack
+  clear(){
+    this.items = []
+    this.count = 0
+    console.log('Stack cleared')
+    return this.items
+  }
 }
 
 const stack = new Stack ()
 
+stack.isEmpty()
 stack.push(100)
 stack.push(200)
+stack.peek()
 stack.push(300)
-
+console.log(stack.print())
 stack.pop()
 stack.pop()
+stack.clear()
+console.log(stack.print())
+stack.size()
+stack.isEmpty()
